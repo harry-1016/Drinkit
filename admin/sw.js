b64777901bfb01489c40a19e61d98aae51a7ca57
harry-1016 <68921827+harry-1016@users.noreply.github.com>
2026-05-28 16:35:00 +0530

    Add files via upload

---
+++ admin/sw.js
@@ -0,0 +1,4 @@
+const CACHE='deliveryos-admin-v1';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
