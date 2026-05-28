8f4fa2791aebca2062a2abd4453db68a0db150b6
harry-1016 <68921827+harry-1016@users.noreply.github.com>
2026-05-28 16:33:51 +0530

    Add files via upload

---
+++ delivery/sw.js
@@ -0,0 +1,4 @@
+const CACHE='driveos-v1';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
